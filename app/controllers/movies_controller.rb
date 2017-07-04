280106974eefa69f4a1f1e562d6f6258c604c034
Sichang Park <spark@troy.edu>
2017-07-04 04:25:49 +0000

    4.7.1 added create_method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,22 @@
 class MoviesController < ApplicationController
+  def create
+params.require(:movie)
+permitted = params[:movie].permit(:title,:rating,:release_date)
+# shortcut: permitted = params.require(:movie).permit(:title,:rating,:release_date)
+# rest of code...
+# using permitted instead of params[:movie]
+# e.g. @movie = Movie.create!(permitted)
+end
   def index
     @movies = Movie.all
+  end
+  def show
+  id = params[:id] # retrieve movie ID from URI route
+  @movie = Movie.find(id) # look up movie by unique ID
+  # will render app/views/movies/show.html.haml by default
+  end
+  def new
+    @movie = Movie.new
+    #default: render 'new' template
   end
 end
